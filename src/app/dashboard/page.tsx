b02e6ec44598398a8c8a1ea6d5ceb11024839bc8
Blasius Yonas Vikariandi <95988741+Yovieee@users.noreply.github.com>
2025-07-11 13:28:52 +0000

    cek 1 2

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,10 +3,10 @@ import { useSidebarStore } from "@/components/sidebar/store/sidebar.store";
 import { Button } from "primereact/button";
 import { DataTable } from "primereact/datatable";
 
-export default function DashboardPage(){
+export default function DashboardPage() {
     console.log("info")
-    const toggleSidebar= useSidebarStore(state => state.toggleSidebar)
+    const toggleSidebar = useSidebarStore(state => state.toggleSidebar)
     return <>
-        <Button severity="danger"  label="heloy" onClick={toggleSidebar}/>
+        <Button severity="danger" label="heloy" onClick={toggleSidebar} />
     </>
 }
